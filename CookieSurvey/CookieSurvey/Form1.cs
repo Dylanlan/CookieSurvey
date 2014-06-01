2b3d4189d9710c55ae36fad8976bf9df27c0d584
Dylan Stankievech <dylan.stank@gmail.com>
2014-06-01 10:59:41 -0600

    Added documentation and comments

--- CookieSurvey/CookieSurvey/Form1.cs
+++ CookieSurvey/CookieSurvey/Form1.cs
@@ -12,33 +12,62 @@ namespace CookieSurvey
 {
     public partial class Form1 : Form
     {
+        // Used to save information about the receipt
         public static int month;
         public static int day;
         public static int year;
         public static int hour;
         public static int minute;
         public static bool MWF;
+
+        // Used to see whether the form was cancelled
         public static bool abort;
 
+        /// <summary>
+        /// Initializes the form for receipt input
+        /// </summary>
         public Form1()
         {
             abort = true;
             InitializeComponent();
-
         }
 
-        void Form1_Shown(object sender, System.EventArgs e)
+        /// <summary>
+        /// Called when the Form should be shown, ensures that
+        /// it is displayed properly
+        /// </summary>
+        /// <param name="sender">
+        /// The caller of this method
+        /// </param>
+        /// <param name="e">
+        /// The arguments associated with this operation
+        /// </param>
+        void Form1_Shown(object sender, EventArgs e)
         {
+            // Gets the current process and window handle to focus it
             var process = Process.GetCurrentProcess();
             var handle = process.MainWindowHandle;
             Program.SetForegroundWindow(handle);
             Program.ShowWindow(handle, 9); // restore
         }
 
-
+        /// <summary>
+        /// Called when the 'Ok' button is clicked. Saves the
+        /// receipt information, and signals that the form was not
+        /// cancelled
+        /// </summary>
+        /// <param name="sender">
+        /// The caller of this method
+        /// </param>
+        /// <param name="e">
+        /// The arguments associated with this operation
+        /// </param>
         private void button1_Click(object sender, EventArgs e)
         {
+            // Form was not cancelled
             abort = false;
+
+            // Save receipt information
             month = this.DatePicker.Value.Month;
             day = this.DatePicker.Value.Day;
             year = this.DatePicker.Value.Year;
@@ -46,19 +75,53 @@ namespace CookieSurvey
             minute = (int)this.MinutePicker.Value;
             MWF = this.CheckBox.Checked;
 
+            // Close this form
             this.Close();
         }
 
+        /// <summary>
+        /// When the numericUpDown is entered, it selects all the
+        /// current text, so it is easy to input the receipt information
+        /// </summary>
+        /// <param name="sender">
+        /// The caller of this method
+        /// </param>
+        /// <param name="e">
+        /// Arguments for this operation
+        /// </param>
         private void HourPicker_Enter(object sender, EventArgs e)
         {
+            // Highlights all current text, easy to overwrite with correct information
             this.HourPicker.Select(0, this.HourPicker.Text.Length);
         }
 
+        /// <summary>
+        /// When the numericUpDown is entered, it selects all the
+        /// current text, so it is easy to input the receipt information
+        /// </summary>
+        /// <param name="sender">
+        /// The caller of this method
+        /// </param>
+        /// <param name="e">
+        /// Arguments for this operation
+        /// </param>
         private void MinutePicker_Enter(object sender, EventArgs e)
         {
+            // Highlights all current text, easy to overwrite with correct information
             this.MinutePicker.Select(0, this.MinutePicker.Text.Length);
         }
 
+        /// <summary>
+        /// Called when the 'Cancel' button is clicked. Since the abort field is only
+        /// set to false from clicking 'Ok', it should still be true. So it just
+        /// closes the form.
+        /// </summary>
+        /// <param name="sender">
+        /// The caller of this method
+        /// </param>
+        /// <param name="e">
+        /// The arguments associated with this operation
+        /// </param>
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
--- CookieSurvey/CookieSurvey/Program.cs
+++ CookieSurvey/CookieSurvey/Program.cs
@@ -32,22 +32,33 @@ namespace CookieSurvey
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Tries to find the subway cookies tab in chrome
+            // Focuses chrome for input if it found it
+            // Exits if it can't find it
             FocusChrome();
+
+            // To get to the input box
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait("{TAB}");
+
+            // The store number
             SendKeys.SendWait("25544");
+
+            // Submits to get to the survey
             SendKeys.SendWait("{ENTER}");
 
-            var form = new Form1();
-            Application.Run(form);
+            // Opens a form to get the date of the receipt
+            Application.Run(new Form1());
 
-
+            // If the form was cancelled
             if (Form1.abort)
             {
                 return;
             }
 
+            // Get the saved information about the receipt
             string month = Form1.month.ToString();
             string day = Form1.day.ToString();
             string year = Form1.year.ToString();
@@ -58,25 +69,29 @@ namespace CookieSurvey
             string hourString = hour.ToString();
             string minuteString = minute.ToString();
 
+            // If hour is 1 to 9, needs a 0 in front of it
             if (hour < 10 && hour > 0)
             {
                 hourString = "0" + hourString;
             }
 
+            // If minute is 1 to 9, needs a 0 in front of it
             if (minute < 10 && minute > 0)
             {
                 minuteString = "0" + minuteString;
             }
 
+            // Finds the survey in chrome again, or exits if not found
             FocusChrome();
 
-
+            // 5 tabs to get to the date input
             for (int i = 0; i < 5; i++)
             {
                 SendKeys.SendWait("{TAB}");
                 System.Threading.Thread.Sleep(100);
             }
 
+            // Input the saved date and time of the receipt
             SendKeys.SendWait(day);
             SendKeys.SendWait("/");
             SendKeys.SendWait(month);
@@ -87,7 +102,7 @@ namespace CookieSurvey
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait(minuteString);
 
-
+            // Chooses the radio button 9 for the first 4 questions
             for (int j = 0; j < 4; j++)
             {
                 SendKeys.SendWait("{TAB}");
@@ -97,6 +112,7 @@ namespace CookieSurvey
                 }
             }
 
+            // Chooses the radio button 8 for the next 3 questions
             for (int j = 0; j < 3; j++)
             {
                 SendKeys.SendWait("{TAB}");
@@ -106,24 +122,32 @@ namespace CookieSurvey
                 }
             }
 
+            // Chooses the radio button 9 for the last question
             SendKeys.SendWait("{TAB}");
             for (int i = 0; i < 9; i++)
             {
                 SendKeys.SendWait("{RIGHT}");
             }
 
+            // Chooses the drop down 'No' for if there's a compliment
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait("n");
 
+            // Chooses the drop down 'No' for if there's a complaint
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait("n");
 
+            // Chooses the drop down 20 for fast food visits per month
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait("20");
 
+            // Chooses the drop down 20 for subway visits per month
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait("20");
 
+            // They try to make sure you can only use the same email once for every
+            // 24 hours, so I just alternate email addresses
+            // Depending on if I will be handing in the receipt on a MWF or TR
             SendKeys.SendWait("{TAB}");
             if (MWF)
             {
@@ -134,26 +158,34 @@ namespace CookieSurvey
                 SendKeys.SendWait("dylan.stank@gmail.com");
             }
 
-
+            // No, I don't want any 'valuable' offers
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait("n");
 
+            // No, I don't want to be contacted
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait("n");
 
+            // Will submit the survey
             SendKeys.SendWait("{TAB}");
             //SendKeys.SendWait("{ENTER}");
         }
 
-
+        /// <summary>
+        /// Checks for the subway survey tab in google chrome. If one exists, it
+        /// will focus it for input. If it couldn't find it, it will display a
+        /// message box and exit the program.
+        /// </summary>
         public static void FocusChrome()
         {
+            // Get all current processes with the name 'chrome'
             Process[] procs = Process.GetProcessesByName("chrome");
 
+            // If there were no chrome processes, say so and exit
             if (procs.Length == 0)
             {
-                Console.WriteLine("Google Chrome is not currently open");
-                return;
+                MessageBox.Show("Google Chrome is not currently open");
+                Environment.Exit(0);
             }
 
             List<string> titles = new List<string>();
@@ -162,8 +194,10 @@ namespace CookieSurvey
             int id = 0;
             bool foundSubway = false;
 
+
             foreach (Process p in procs)
             {
+                // Break if any process has the subway title
                 if (p.MainWindowTitle == "SUBWAY Customer Survey - Google Chrome")
                 {
                     foundSubway = true;
@@ -173,6 +207,7 @@ namespace CookieSurvey
                 }
             }
 
+            // If no process had the correct title, say so and exit
             if (!foundSubway)
             {
                 MessageBox.Show("Couldn't find a Subway cookies tab in google chrome!");
@@ -181,14 +216,15 @@ namespace CookieSurvey
 
             bool isMinimized = IsIconic(hWnd);
 
+            // If the window was minimized, restore it
             if (isMinimized)
             {
-                ShowWindow(hWnd, 9); // restore
+                ShowWindow(hWnd, 9);
                 System.Threading.Thread.Sleep(100);
             }
 
-            SetForegroundWindow(hWnd); // set focus to Google Chrome
-            //SendKeys.SendWait("+{ESC}"); // pop up Chrome Task Manager window     
+            // Set focus to Google Chrome
+            SetForegroundWindow(hWnd);    
             System.Threading.Thread.Sleep(100);
         }
     }
